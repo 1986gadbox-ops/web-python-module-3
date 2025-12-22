a76ea246f53383ed68bebda182b6b85519dc6eb8
1986gadbox-ops <1986gadbox@gmail.com>
2025-12-22 19:19:14 +0300

    my work

---
+++ module-1/classwork/task_1.py
@@ -0,0 +1,9 @@
+num_str1 = input("Ведите число: ")
+num_str2 = input("Ведите второе число: ")
+num_str3 = input("Ведите третье число: ")
+
+num1 = int(num_str1)
+num2 = int(num_str2)
+num3 = int(num_str3)
+
+print (num1, num2, num3, sep ='')

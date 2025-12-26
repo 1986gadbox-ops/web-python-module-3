3a32fb52de04d9fedd00638f3e719f9f029bfb6f
1986gadbox-ops <1986gadbox@gmail.com>
2025-12-26 19:58:36 +0300

    домашнее задание

---
+++ module-1/homework/task_1.py
@@ -0,0 +1,55 @@
+# задание 1
+
+# n1 = input("Введите первое число: ")
+# n2 = input("Введите второе число: ")
+# n3 = input("Введите третье число: ")
+
+# num1 = int(n1)
+# num2 = int(n2)
+# num3 = int(n3)
+
+# amount = (num1 + num2 + num3)
+# pro = (num1 * num2 * num3)
+
+# print(f"сумма: {amount}, произведение: {pro}")
+
+
+# задание 2
+
+# n1 = input("Введите зарплату за месяц: ")
+# n2 = input("Введите сумму месянчого платежа по кредиту в банке: ")
+# n3 = input("Введите задолженность за коммунальные услуги: ")
+
+# num1 = int(n1)
+# num2 = int(n2)
+# num3 = int(n3)
+
+# amount = (num1 - num2 - num3)
+
+# print(f"после всех выплат у Вас останется: {amount} ")
+
+
+
+# задание 3
+
+# n1 = input("Введите длинну первой диагонали: ")
+# n2 = input("Введите длинну второй диагонали: ")
+
+# num1 = int(n1)
+# num2 = int(n2)
+
+# square =(num1 * num2 / 2)
+
+# print(f"площадь ромба равна: {square} сантиметров")
+
+
+# задание 4
+
+# print("To be \nor not \nto be")
+
+
+
+
+# задание 5
+
+# print('"Life is what happens \n      when\n           you’re busy making other plans"\n                            John Lennon')
